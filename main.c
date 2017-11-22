68eb3e44d67737eb3530332527e982d3aade50af
Javier Castro <jaca2899@gmail.com>
2017-11-22 16:09:11 -0500

    modified:   main.c

--- main.c
+++ main.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 int main(int argc, char **argv)
 {
-	SDL_Window *window;
+	SDL_Window *window = NULL;
 
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Surface* surface = NULL;
 
-	window = SDL_CreateWindow("Cythen",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640, 480,
-		SDL_WINDOW_OPENGL);
-
-	if(window == NULL)
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf("Could not create window: %s\n", SDL_GetError());
-		return 1;
+		printf("SDL could not initialize! SDL_Error: %s\n",
+			SDL_GetError());
 	}
+	else
+	{
+		window = SDL_CreateWindow("Cythen",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
 
-	SDL_Delay(3000);
+		if(window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n",
+					SDL_GetError());
+		}
+		else
+		{
+			surface = SDL_GetWindowSurface(window);
+
+			SDL_FillRect(surface, NULL,
+				SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+			SDL_UpdateWindowSurface(window);
+
+			SDL_Delay(3000);
+		}
+	}
 
 	SDL_DestroyWindow(window);
 	SDL_Quit();
