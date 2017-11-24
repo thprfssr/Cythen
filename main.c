cccbbb3312d88c46d071623f24194ee9082b6969
Javier Castro <jaca2899@gmail.com>
2017-11-23 22:02:35 -0500

    modified:   main.c

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 144
 #define DESIRED_FPS 60
 
 static bool QUIT = false;
@@ -12,7 +14,7 @@ SDL_Window* open_window()
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf("SDL could not initialize!SDL_Error: %s\n",
+		printf("SDL could not initialize! SDL_Error: %s\n",
 				SDL_GetError());
 	}
 	else
@@ -33,9 +35,36 @@ SDL_Window* open_window()
 	return window;
 }
 
+SDL_Surface* create_game_surface()
+{
+	SDL_Surface *surface;
+	Uint32 rmask, gmask, bmask, amask;
+
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
+#else
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+#endif
+
+	surface = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32,
+				       rmask, gmask, bmask, amask);
+	if (surface == NULL)
+	{
+		SDL_Log("SDL_CreateRGBSurface() failed: %s\n",
+			SDL_GetError());
+		exit(1);
+	}
+}
+
 void handle_event(SDL_Event event)
 {
-	switch(event.type)
+	switch (event.type)
 	{
 		case SDL_QUIT:
 			QUIT = true;
